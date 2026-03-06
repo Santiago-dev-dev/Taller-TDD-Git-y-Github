bd9ba88f2339cd915833890999db84c7d30e7861
jrdiaz82 <snvillamizar37@ucatolica.edu.co>
2026-03-06 12:16:50 -0500

    Agregar archivos par

---
+++ tests/test_par.py
@@ -0,0 +1,12 @@
+from funciones.par es_par
+resutlado_1 = es_par(4)
+if resutlado_1 = es_par(4):
+    print("Test 1 aprobado: 4 es par")
+else:
+    print("Test 1 fallo")
+
+resultado_2 = es_par(5)
+if resultado_2 == False:
+    print("Test 2 aprobado: 5 es impar")
+else:
+    print("Test 2 fallo")
